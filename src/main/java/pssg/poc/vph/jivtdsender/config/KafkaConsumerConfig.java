41dba3570e23edd3f23c7e5e6c2d50a7d4b94adc
Echo Liang <echo.2.liang@gov.bc.ca>
2018-11-13 11:04:28 -0800

    config change to allow multiple consumers consuming same messages

--- src/main/java/pssg/poc/vph/jivtdsender/config/KafkaConsumerConfig.java
+++ src/main/java/pssg/poc/vph/jivtdsender/config/KafkaConsumerConfig.java
@@ -38,7 +38,10 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        
+        //https://stackoverflow.com/questions/30899163/how-do-i-use-multiple-consumers-in-kafka
+        //to allow multiple consumers to consume same messages
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "smallest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
